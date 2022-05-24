8651e9d9674bbcbdea35697ce873f727ae4e5c00
James Tan <james_tan97@outlook.com>
2022-05-24 19:30:51 -0400

    Support multiple servers

--- lab3/Client.java
+++ lab3/Client.java
@@ -4,20 +4,27 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Client {
     DatagramSocket c;
+    List<ServerDetails> serverList;
 
-    void run() throws IOException {
+    public Client() throws IOException {
         c = new DatagramSocket();
         c.setBroadcast(true);
-       
+
+        serverList = new ArrayList<>();
+    }
+
+    void peerDiscovery() throws IOException {
         byte[] sendData = "DISCOVER_FUIFSERVER_REQUEST".getBytes();
        
-        //Try the 255.255.255.255 first
         DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("255.255.255.255"), 8888);
         c.send(sendPacket);
         System.out.println(getClass().getName() + " >>> Request packet sent to: 255.255.255.255 (DEFAULT)");
@@ -25,24 +32,60 @@ public class Client {
         //Wait for a response
         byte[] recvBuf = new byte[15000];
         DatagramPacket receivePacket = new DatagramPacket(recvBuf, recvBuf.length);
-        c.receive(receivePacket);
+
+
+        while (true) {
+            c.receive(receivePacket);
         
-        //We have a response
-        String hostAddress = receivePacket.getAddress().getHostAddress();
-        String msg = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
+            // We have a response
+            String hostAddress = receivePacket.getAddress().getHostAddress();
+            String msg = new String(receivePacket.getData(), receivePacket.getOffset(), receivePacket.getLength());
+    
+            System.out.println(getClass().getName() + " >>> Broadcast response from server: " + hostAddress + " msg: " + msg);
+    
+            Pattern r = Pattern.compile("ALIVE\\s+(\\w+)\\s+(\\d+)");
+            Matcher m = r.matcher(msg);
+    
+            // TODO: Handle the case where the message is malformed
+            if (m.find()) {
+                int port = Integer.parseInt(m.group(2));
+                String name = m.group(1);
+    
+                System.out.println(String.format("%s >>> Identified server %s %d", getClass().getName(), hostAddress, port));
+    
+                serverList.add(new ServerDetails(hostAddress, port, name));
+            }
+        }
+    }
 
-        System.out.println(getClass().getName() + " >>> Broadcast response from server: " + hostAddress + " msg: " + msg);
+    void run() throws IOException {
+        new Thread(() -> {
+            try {
+                peerDiscovery();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }).start();
 
-        Pattern r = Pattern.compile("ALIVE\s+(\\w+)\s+(\\d+)");
-        Matcher m = r.matcher(msg);
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
-        // TODO: Handle the case where the message is malformed
-        if (m.find()) {
-            int port = Integer.parseInt(m.group(2));
+        if (serverList.size() == 0) {
+            System.out.println(getClass().getName() + " >>> NO server found");
+        } else {
+            System.out.println(String.format("%s >>> %d servers found", getClass().getName(), serverList.size()));
 
-            System.out.println(String.format("%s >>> Identified server %s %d", getClass().getName(), hostAddress, port));
+            // Get random server
+            Random rand = new Random();
+            ServerDetails details = serverList.get(rand.nextInt(serverList.size()));
 
-            TCPClient tcpClient = new TCPClient(hostAddress, port);
+            System.out.println(String.format("%s >>> Connecting to random server %s", getClass().getName(), details.name));
+
+            TCPClient tcpClient = new TCPClient(details.address, details.port);
 
             String response = tcpClient.recvln();
 
@@ -76,6 +119,17 @@ public class Client {
         public String runCommand(String command) {
             sendln(command);
             return recvln();
+        }
+    }
+
+    private class ServerDetails {
+        String address;
+        int port;
+        String name;
+        public ServerDetails(String address, int port, String name) {
+            this.address = address;
+            this.port = port;
+            this.name = name;
         }
     }
 }
--- lab3/ServerLauncher.java
+++ lab3/ServerLauncher.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 
 public class ServerLauncher {
     public static void main(String[] args) throws IOException {
-        new Server(0, "R1").start();
+        String serverName = "R1";
+
+        if( args.length > 0)
+            serverName = args[0];
+
+        new Server(0, serverName).start();
 
         System.out.println("\nPress hit ENTER if you wish to stop the servers. Note that the service may NOT stop immediately.");
         new Scanner(System.in).nextLine(); // waiting for EOL from the console to terminate
