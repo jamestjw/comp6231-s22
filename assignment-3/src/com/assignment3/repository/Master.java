ee64218d6f8727e61368f7f472ee645071046e36
James Tan <james_tan97@outlook.com>
2022-07-01 17:25:50 -0400

    Implement uploading of files in Master node class

--- assignment-3/src/com/assignment3/repository/Master.java
+++ assignment-3/src/com/assignment3/repository/Master.java
@@ -1,15 +1,40 @@
 // Unable to run MPI with this :( I hate java
 // package com.assignment3.repository;
 
+import java.io.InputStream;
 import java.nio.ByteBuffer;
+import java.nio.file.DirectoryStream.Filter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import mpi.MPI;
 
 public class Master {
+    int numSlaves;
+    int remainingAvailableClusters;
+    HashMap<Integer, ArrayList<Integer>> slaveAvailableClusters;
+
+    public Master(int numSlaves) {
+        this.numSlaves = numSlaves;
+        this.remainingAvailableClusters = numSlaves * Slave.NUM_CLUSTERS;
+        this.slaveAvailableClusters = new HashMap<>();
+
+        for (int i = 1; i <= numSlaves; i++) {
+            ArrayList<Integer> l = new ArrayList<>(
+                    IntStream.range(0, Slave.NUM_CLUSTERS).boxed().collect(Collectors.toList()));
+            this.slaveAvailableClusters.put(i, l);
+        }
+
+        writeLog(String.format("Active with %d slave nodes.", numSlaves));
+    }
+
     /*
      * Handles a file upload
      */
-    public void upload() {
+    public void upload(String filename, int filesize, InputStream data) {
         int dest = 1;
         int destCluster = 4;
         byte buffer_send[] = generateByteArray(destCluster, Slave.CLUSTER_SIZE);
@@ -37,5 +62,43 @@ public class Master {
 
     private void writeLog(String s) {
         System.out.println(String.format("<MASTER> %s", s));
+    }
+
+    private synchronized List<StorageLocation> allocateStorageLocations(int filesize) {
+        int numClustersRequired = (int) Math.ceil((double) filesize / Slave.CLUSTER_SIZE);
+
+        if (numClustersRequired > remainingAvailableClusters) {
+            throw new InsufficientStorageException("Not enough storage");
+        }
+    }
+
+    private class FileEntry {
+        private String name;
+        private int size;
+        private List<StorageLocation> directory;
+
+        FileEntry(String name, int size) {
+            this.name = name;
+            this.size = size;
+            this.directory = new ArrayList<>();
+        }
+
+        /*
+         * This should be called in the right order, i.e. the first
+         * time this is called the location data would correspond to the
+         * first file part.
+         */
+        void addLocation(int slaveRank, int clusterNumber) {
+            directory.add(new StorageLocation(slaveRank, clusterNumber));
+        }
+    }
+
+    record StorageLocation(int slaveRank, int clusterNumber) {
+    }
+
+    public class InsufficientStorageException extends Exception {
+        public InsufficientStorageException(String errorMessage) {
+            super(errorMessage);
+        }
     }
 }
