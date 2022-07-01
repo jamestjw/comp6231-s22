4756c0fd9abdc9b06b4c9f66f0e3143a683c840f
James Tan <james_tan97@outlook.com>
2022-07-01 16:42:17 -0400

    Add dummy code to write data to Slave node

--- assignment-3/README.md
+++ assignment-3/README.md
@@ -1,7 +1,7 @@
 # Assignment 3 - COMP 6231
 
 ## How to run?
- ### Server
+### Server
 ```bash
-(MPJ_HOME=mpj-v0_44 cd src/com/assignment3/repository/ && javac -cp $MPJ_HOME/lib/mpj.jar Server.java && $MPJ_HOME/bin/mpjrun.sh -np 2 Server)
+(MPJ_HOME=mpj-v0_44 cd src/com/assignment3/repository/ && java-cp $MPJ_HOME/lib/mpj.jar Server.java Slave.java Master.java && $MPJ_HOME/bin/mpjrun.sh -np 4 Server)
 ```
--- assignment-3/src/com/assignment3/repository/Server.java
+++ assignment-3/src/com/assignment3/repository/Server.java
@@ -1,12 +1,15 @@
 // Unable to run MPI with this :( I hate java
 // package com.assignment3.repository;
 
-import mpi.*;
+import java.nio.ByteBuffer;
+
+import mpi.MPI;
+// import Slave;
 
 public class Server {
     public static void main(String[] args) throws Exception {
-        if (args.length < 5) {
-            System.out.println("usage: $MPJ_HOME/bin/mpjrun.sh -np 2 -cp output MPJSendReceive <integer> <integer> ");
+        if (args.length < 3) {
+            System.out.println("usage: $MPJ_HOME/bin/mpjrun.sh -np 2 -cp output Server");
             return;
         }
 
@@ -14,44 +17,22 @@ public class Server {
         int rank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
         int tag = 1;
-        int dst;
 
-        if (rank == 1) {
-            dst = 0;
-            int buffer_rcv[] = new int[1];
-            int buffer_snt[] = new int[1];
-            buffer_snt[0] = Integer.parseInt(args[4]);
-            MPI.COMM_WORLD.Send(buffer_snt, 0, 1, MPI.INT, dst, tag);
-            System.out.println("My ID is <" + rank + "> sent # " + buffer_snt[0]);
-
-            // TODO: implment Receive of the sum
-            MPI.COMM_WORLD.Recv(buffer_rcv, 0, 1, MPI.INT, dst, tag);
-            System.out.println("My ID is <" + rank + "> sum is " + buffer_rcv[0]);
-
+        if (size < 2) {
+            System.err.println("At least two nodes are required to run the repository.");
+            System.exit(1);
         }
 
-        else if (rank == 0) {
-            dst = 1;
-            int buffer_snt[] = new int[1];
-            int buffer_rcv[] = new int[1];
-
-            MPI.COMM_WORLD.Recv(buffer_rcv, 0, 1, MPI.INT, dst, tag);
-            System.out.println("My ID is <" + rank + "> received # " + buffer_rcv[0]);
-            // TODO impleent addition of given integers
-            int res = Integer.parseInt(args[3]);
-            res += buffer_rcv[0];
-
-            buffer_snt[0] = res;
-
-            // TODO implement Send of the sum.
-            //
-            System.out.println("My ID is <" + rank + "> sent sum " + buffer_snt[0]);
-            MPI.COMM_WORLD.Send(buffer_snt, 0, 1, MPI.INT, dst, tag);
+        // Master process
+        if (rank == 0) {
+            Master m = new Master();
+            m.upload();
         }
-
+        // Slave process
         else {
-            System.out.println("My ID is <" + rank + ">. I'm not doing anything");
+            new Slave(rank).run();
         }
+
         MPI.Finalize();
     }
 }
