87df52c42d6ad4363c3aa68becd6a968c07818c6
James Tan <james_tan97@outlook.com>
2022-07-12 20:04:19 -0400

    Add list files feature to client app

--- assignment-3/src/com/assignment3/Client.java
+++ assignment-3/src/com/assignment3/Client.java
@@ -1,16 +1,55 @@
-import java.io.ByteArrayInputStream;
 import java.rmi.Naming;
+import java.util.Scanner;
+import java.util.List;
+import java.rmi.RemoteException;
 
 public class Client {
     public static void main(String[] argv) throws Exception {
         Repository r = (Repository) Naming.lookup("rmi://localhost:6231/REPOSITORY");
 
-        String filename = "test222.txt";
-        byte[] data = "testing".getBytes();
-        int filesize = data.length;
-        r.upload(filename, filesize, data);
-        for (String name: r.listFiles()) {
-            System.out.println(name);
+        // String filename = "test222.txt";
+        // byte[] data = "testing".getBytes();
+        // int filesize = data.length;
+        // r.upload(filename, filesize, data);
+        // for (String name: r.listFiles()) {
+        //     System.out.println(name);
+        // }
+        boolean running = true;
+        Scanner input = new Scanner(System.in);
+
+        while (running)
+        {
+            System.out.println("1. List files");
+            System.out.println("2. Upload file");
+            System.out.println("Input 'q' to exit.");
+            String in = input.next();
+            switch (in) {
+                case "1":
+                    handleListFiles(r);
+                    break;
+                case "2":
+                    handleUploadFile(r);
+                    break;
+                case "q":
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid command.");
+                    break;
+            }
         }
+    }
+
+    private static void handleListFiles(Repository r) throws RemoteException {
+        List<String> files = r.listFiles();
+        System.out.println(String.format("Found %d files.", files.size()));
+        for (int i = 0; i < files.size(); i++)
+            System.out.println(String.format("%d. %s", i + 1, files.get(i)));
+
+        System.out.println("");
+    }
+
+    private static void handleUploadFile(Repository r) {
+
     }
 }
